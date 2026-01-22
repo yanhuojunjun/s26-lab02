7d02cd637cf068477e510afa1ff74c47ebe8604c
yanhuojunjun <yanhuojunjun@gmail.com>
2026-01-21 20:49:45 -0500

    Encapsulation for Java 1. Change interface for the Shape class : Add getArea() function 2. Change the Renderer class : Pass in the Shape type and use the getArea() interface it provides, so that the Renderer is only dependent on the Shape class 3. Change main: Define and draw multiple renderers receiving different implementations of shape

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,19 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(2, 3);
+        Circle circle = new Circle(1);
+        Square square = new Square(3);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer1 = new Renderer(rectangle);
+        Renderer renderer2 = new Renderer(circle);
+        Renderer renderer3 = new Renderer(square);
 
-        renderer.draw();
+        renderer1.draw();
+        renderer2.draw();
+        renderer3.draw();
     }
 }
